301b2015ce50418b698e38c3212d145e6961a22a
Reforcae <reforcae@gmail.com>
2016-01-16 14:55:38 -0300

    Configure path, bodyParser, Corser

--- dev/server/config/express.js
+++ dev/server/config/express.js
@@ -1,4 +1,6 @@
 var express    = require("express");
+var corser     = require("corser");
+var bodyParser = require("body-parser");
 
 module.exports = function() {
 
@@ -9,6 +11,17 @@ module.exports = function() {
     app.set( 'view engine', 'ejs');
 
     app.set( 'views', './dev/server/views'  );
+
+    app.use( express.static( config.rootPath +'/frontend' ) );
+
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use(bodyParser.json());
+
+    app.use(corser.create({
+        methods: corser.simpleMethods.concat(["PUT", "DELETE"]),
+        requestHeaders: corser.simpleRequestHeaders.concat(["Authorization","X-List-Total","x-auth",'X-Acess-Token'])
+    }));
 
     return app;
 

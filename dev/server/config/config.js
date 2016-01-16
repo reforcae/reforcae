a3bec85364b1e8bdb92dc88874410d4d599c1499
Reforcae <reforcae@gmail.com>
2016-01-16 14:13:24 -0300

    Create config.js

---
+++ dev/server/config/config.js
@@ -0,0 +1,13 @@
+var path     = require('path'),
+    rootPath = path.normalize(__dirname + '/../../');
+
+module.exports = {
+  development : {
+    rootPath : rootPath,
+    db : 'mongodb://localhost:27017/agenda',
+    port : process.env.PORT || 4000
+  },
+  metaadmin : {
+    token : "dGhpc2lzYXNlY3JldHRva2VubGVv"
+  } 
+};

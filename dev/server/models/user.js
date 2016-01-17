513a5086f586a3eca740d6af0411058c7b4be979
Reforcae <reforcae@gmail.com>
2016-01-16 21:01:48 -0300

    Create Model user

---
+++ dev/server/models/user.js
@@ -0,0 +1,39 @@
+var mongoose = require("mongoose");
+
+module.exports = function () {
+
+    var STATUS = ['ONLINE','OFFLINE',"BUSY"];
+
+    var userSchema = new mongoose.Schema({
+        first_name    : { type : String, trim : true },
+        last_name     : { type : String, trim : true },
+        roles         : [ String ],
+        phone_number  : String,
+        username    : { type : String, trim : true },
+        salt        : { type : String },
+        hashed_pwd  : { type : String }, 
+        last_access : Date,
+        online      : { type : String, enum : STATUS },
+        disable     : { type : Boolean, default : false },
+        email : {
+            type     : String,
+            required : true,
+            index    : { unique : true  },
+            trim     : true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    });
+
+    userSchema.set( 'toJSON', {
+        virtuals: true,
+        transform: function (doc, ret, options) {
+            delete ret._id;
+            delete ret.__v;
+        }
+    });
+
+    return mongoose.model( "user", userSchema );
+};

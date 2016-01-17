881b1d3db76d7879e4aa2f68a524e0d996eb8a91
Reforcae <reforcae@gmail.com>
2016-01-16 21:13:30 -0300

    Define Route API V1 and Add Express Loader

--- dev/server/config/express.js
+++ dev/server/config/express.js
@@ -1,8 +1,9 @@
 var express    = require("express");
 var corser     = require("corser");
+var load       = require("express-load");
 var bodyParser = require("body-parser");
 
-module.exports = function(config) {
+module.exports = function ( config ) {
 
     var app = express();
 
@@ -14,14 +15,21 @@ module.exports = function(config) {
 
     app.use( express.static( config.rootPath +'/frontend' ) );
 
-    app.use(bodyParser.urlencoded({extended: true}));
+    app.use( bodyParser.urlencoded( { extended : true } ) );
 
-    app.use(bodyParser.json());
+    app.use( bodyParser.json() );
 
-    app.use(corser.create({
+    app.use( corser.create({
         methods: corser.simpleMethods.concat(["PUT", "DELETE"]),
         requestHeaders: corser.simpleRequestHeaders.concat(["Authorization","X-List-Total","x-auth",'X-Acess-Token'])
     }));
+    
+    load( 'models', { cwd : './dev/server' } )
+        .then('services')
+        .into(app)
+
+    var apiV1 = require('./../apis/v1');
+    app.use( '/apis/v1/', apiV1(app) );
 
     return app;
 

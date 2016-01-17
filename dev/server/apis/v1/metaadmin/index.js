b7c1bbcd68e85b381e1af75ad083fb10c7cd1277
Reforcae <reforcae@gmail.com>
2016-01-16 21:46:31 -0300

    Intercept token and verify

--- dev/server/apis/v1/metaadmin/index.js
+++ dev/server/apis/v1/metaadmin/index.js
@@ -8,6 +8,20 @@ function this_module ( app ) {
 
     var userModel        = app.models.user;
     var metaAdminToken   = config.metaadmin.token;
+    var metaAdminService = app.services.metaadminService;
+
+    /**
+     * 
+     * Verify if the post has a token secret. This allow access for route metaadmin/ 
+     * @return {next or {}} next to middlware or return error
+     * 
+     */
+    api.use( function ( req, res, next ) {
+        ( req.headers.authorization && req.headers.authorization.split(' ')[1] == metaAdminToken ) 
+        ? next()
+        : res.status(401).json({ error: 401, message: 'Unauthorized'});
+    });
+
 
     api.route('/')
         .post( function ( req, res ) {

cf1ca0dbee962eadb816a3392b8da76ea4d3f386
Reforcae <reforcae@gmail.com>
2016-01-16 21:45:01 -0300

    Create util encrypt

---
+++ dev/server/utils/encrypt.js
@@ -0,0 +1,20 @@
+var crypto = require('crypto');
+
+function createSalt() {
+    return crypto.randomBytes(128).toString('base64');
+};
+
+function  hashPwd( salt, pwd ) {
+    var hmac = crypto.createHmac( 'sha1', salt );
+    return hmac.update(pwd).digest('hex');
+};    
+
+function validatePassword( salt, passwordToMatch, hashed_pwd ) {
+   return hashPwd( salt, passwordToMatch ) === hashed_pwd;
+};
+
+module.exports = {
+    'hashPwd' : hashPwd, 
+    'createSalt' : createSalt, 
+    'validatePassword' : validatePassword
+};

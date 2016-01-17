20d5c16da62f6c864afb6ef828e9c7d38cd2e691
Reforcae <reforcae@gmail.com>
2016-01-16 22:39:30 -0300

    Create metaAdmin

--- dev/server/apis/v1/metaadmin/index.js
+++ dev/server/apis/v1/metaadmin/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var api     = express.Router();
 var config  = require('../../../config/config.js');
+var encrypt = require('../../../utils/encrypt.js');
 
 exports = module.exports = this_module;
 
@@ -23,10 +24,38 @@ function this_module ( app ) {
     });
 
 
+    /**
+     * 
+     *  Receive the post with data of user
+     *  -Create salt and generate password
+     *  -Add role admin
+     *  -Create the user admin and return JSON if was success or return err
+     * 
+     */
     api.route('/')
-        .post( function ( req, res ) {
-            console.log( "calling metAdmin");
-        });
+        .post(function ( req, res ) {
+
+            var json        = req.body;
+            json.username   = json.username.toLowerCase();
+            json.salt       = encrypt.createSalt();
+            json.hashed_pwd = encrypt.hashPwd( json.salt, json.hashed_pwd );
+            
+            if ( !userModel.hasOwnProperty( 'roles' ) ) { 
+                json.roles = ['ADMIN'];
+            }
+
+            return metaAdminService.create( userModel, json ).then( function ( adm ) {
+                res.status( 201 ).json("Admin was Created with success");
+            }).catch( function ( e ) {
+                if ( e && e.code === 11000 || e && err.code === 11001 ) {
+                    var valueErr = new Error( 'This email is already used' );
+                    res.status(400);
+                    return res.send({ err : valueErr.toString() });
+                } else {
+                    res.status(500).json({err : 'An error has occurred' + e });
+                }
+            });
+        })
 
     return api;
 };
